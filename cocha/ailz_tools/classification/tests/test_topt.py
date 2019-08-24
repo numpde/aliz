5bb256e328affbbc6cec433a046e7e2308200219
r <21158052+numpde@users.noreply.github.com>
2019-08-24 22:50:42 +0200

    RA: Minor mod

--- cocha/ailz_tools/classification/tests/test_topt.py
+++ cocha/ailz_tools/classification/tests/test_topt.py
@@ -2,11 +2,12 @@
 
 import pytest
 
-from sklearn.utils.estimator_checks import check_transformer_general
+from sklearn.utils.estimator_checks import *
 
-from ailz_tools.classification.topt import CustomEstimator, ThresholdBinarizer
+from ailz_tools.classification.topt import CustomEstimator
 
 
 def test_basic_sklearn_compliance():
-	check_transformer_general("ThresholdBinarizer", ThresholdBinarizer)
+	check_estimators_unfitted("CustomEstimator", CustomEstimator)
+	check_estimator("CustomEstimator", CustomEstimator)
 
